a970539b362a6dbcfc64660b0fa92e39d2785948
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-09-03 23:12:59 -0300

    novos desafios do cap 6

---
+++ desafios_do_proprio_livro/6-2_favorite_numbers.py
@@ -0,0 +1,13 @@
+f_numbers={
+    'Bruno Ribeiro':24,
+    'Rafaela':69,
+    'Vinicius':12,
+    'Samuel':42,
+    'Mateus':40,    
+    }
+
+print('Bruno Ribeiro: '+str(f_numbers['Bruno Ribeiro']))
+print('Rafaela: '+str(f_numbers['Rafaela']))
+print('Vinicius: '+str(f_numbers['Vinicius']))
+print('Samuel: '+str(f_numbers['Samuel']))
+print('Mateus: '+str(f_numbers['Mateus']))
