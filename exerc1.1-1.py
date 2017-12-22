b520096c4b758b0fe036d42b69a8f959cc486d81
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-22 18:00:38 -0200

    Create exerc1.1-1.py

---
+++ exerc1.1-1.py
@@ -0,0 +1,5 @@
+""" 1. Em uma instrução print, o que acontece se você omitir um dos
+    parênteses ou ambos? """
+#Recebemos a segunte mensagem de erro: "SyntaxError: unexpected EOF while parsing"
+#Agora se remover os dois parentesis temos um outro erro de sintaxy semelhante ao anterior
+#SyntaxError: invalid syntax
