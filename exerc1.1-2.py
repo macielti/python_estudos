689d6369d40546670c1a01e40f7b0b22a9eac744
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-22 18:09:04 -0200

    Create exerc1.1-2.py

---
+++ exerc1.1-2.py
@@ -0,0 +1,6 @@
+""" 2. Se tiver tentando imprimir uma 
+    string, o que acontece se omitir 
+    uma das aspas ou ambas? """
+
+#No primeiro caso temos um erro de sintaxe inválida, SyntaxError: invalid syntax.
+#No segundo caso temos o mesmo erro uma vez que o interpretador vai encarar como uma variável e não como uma string.
