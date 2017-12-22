a7f0482167f2e155f28a45f7f86cefafe1d37f1f
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-22 18:21:44 -0200

    Create exerc1.1-4.py

---
+++ exerc1.1-4.py
@@ -0,0 +1,4 @@
+""" 4. Na notação matemática, zeros à esquerda são aceitáveis, como 02.
+    O que acontece se você tentar usar isso no Python? """
+
+#O Python também ignora os zeros à esquerda assim como na notação matemática.
