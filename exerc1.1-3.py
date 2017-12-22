c4948afc6c03cad34942d65f1d60c9f00d684b5b
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-22 18:17:11 -0200

    Create exerc1.1-3.py

---
+++ exerc1.1-3.py
@@ -0,0 +1,8 @@
+""" 3. Você pode usar um sinal de menos para fazer um número negativo
+    com -2. O que acontece se puser um sinal de mais antes de um número?
+    E se escrever assim: 2++2? """
+
+#No primeiro caso nada acontece pois o numero permanece positivo.
+
+#No segundo caso temos 4 como saída pois o interpretador considera o segundo mais como sendo pertencente ao segundo numero. 
+#Depois soma os dois, segundo a ordem de precedência das operações.
