7f91cd8ee0225202a6c2dbc78a6e363eaae6331a
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-23 08:51:32 -0200

    Create exerc1.2-3.py

---
+++ exerc1.2-3.py
@@ -0,0 +1,46 @@
+""" 3. Se você correr 10 quilometros em 42 minutos e 42 segundos, 
+    qual é o seu passo médio (tempo por milha em minutos em segundos)?
+    Qual é a sua velocidade média em milhas por hora? """
+
+
+"""In [19]: segundos = 42*60+42
+
+In [20]: print(segundos)
+2562
+
+In [21]: horas = (segundos /60)/60
+
+In [22]: print(horas)
+0
+
+In [23]: horas = float((segundos /60)/60)
+
+In [24]: print(horas)
+0.0
+
+In [25]: segundos/60
+Out[25]: 42
+
+In [26]: 42/60
+Out[26]: 0
+
+In [27]: 42.0/60.0
+Out[27]: 0.7
+
+In [28]: (10.0/1.61)/0.7
+Out[28]: 8.873114463176574
+
+8.87 milhas por hora
+
+
+In [30]: 8.87/60
+Out[30]: 0.14783333333333332
+
+In [31]: 8.87/60.0
+Out[31]: 0.14783333333333332
+
+In [32]: 0.14/60.0
+Out[32]: 0.0023333333333333335
+
+Faz-se 0.14 milhas em um minuto.
+Faz-se 0.002 milhas por segundo."""
