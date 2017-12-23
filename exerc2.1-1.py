24c279ea6ecc6172e1b61f0c9590afd9211908df
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2017-12-23 09:10:59 -0200

    Create exerc2.1-1.py

---
+++ exerc2.1-1.py
@@ -0,0 +1,30 @@
+#O exercicio pede para droduzir erros envolvendo atribuições e operações com variáveis.
+
+""" In [36]: 42=n
+  File "<ipython-input-36-0784184d4c83>", line 1
+    42=n
+SyntaxError: can't assign to literal
+
+
+In [37]: y=t=54
+
+In [38]: print(y, t)
+(54, 54)
+
+In [39]: print(y, t);
+(54, 54)
+
+In [40]: print(y, t).
+  File "<ipython-input-40-4d2b0738c8ae>", line 1
+    print(y, t).
+                ^
+SyntaxError: invalid syntax
+
+
+In [41]: yt
+---------------------------------------------------------------------------
+NameError                                 Traceback (most recent call last)
+<ipython-input-41-8d37ce3e7282> in <module>()
+----> 1 yt
+
+NameError: name 'yt' is not defined """
